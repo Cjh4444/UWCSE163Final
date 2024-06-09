310a06da060fecc30b5b7e4861390f81dc43b267
Camden Harris <camdenharris@gmail.com>
2024-06-09 00:01:02 -0700

    grouped all stuff, pushing so victor can look at it

--- final submission/analysis.py
+++ final submission/analysis.py
@@ -2,6 +2,7 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
 import cleaning
+import numpy as np
 
 
 def Q1_analysis():
@@ -30,8 +31,6 @@ def Q1_analysis():
 
     popularity_corr = corr_matrix["Streams"].drop("Streams")
 
-    print(popularity_corr)
-
     plt.figure(figsize=(10, 6))
     palette = [
         "#D98672" if value < 0 else "#859FE5"
@@ -46,4 +45,126 @@ def Q1_analysis():
     plt.ylabel("Correlation with # of Streams")
     plt.title("Correlation of Factors with # of Streams")
     plt.show()
-    plt.savefig("")
+    plt.savefig("output/Q1_correlation_graph")
+    plt.close()
+
+
+def Q2_analysis():
+    df, age_groups = cleaning.get_Q2_df()
+
+    fig, [ax1, ax2, ax3] = plt.subplots(ncols=3, figsize=(15, 7))
+
+    axes = [ax1, ax2, ax3]
+
+    for age, axis in zip(age_groups, axes):
+        is_age = df["Age"] == age
+        age_df = df[is_age]
+        total_counts = age_df.count()
+
+        genre_counts = age_df["fav_music_genre"].value_counts()
+
+        axis.bar(genre_counts.index, genre_counts.values)
+
+        axis.set_xlabel("Favorite Music Genre")
+        axis.set_ylabel("Count")
+        axis.set_title(
+            f"{age}",
+            fontdict={"fontsize": 20},
+        )
+
+        print(total_counts)
+
+        axis.tick_params(axis="x", rotation=90)
+
+    plt.show(axis)
+    plt.savefig("output/Q2_bar_graphs")
+    plt.close()
+
+
+def Q3_analysis():
+    tempo_counts, key_counts, duration_counts = cleaning.get_Q3_dfs()
+
+    ax = tempo_counts.plot(
+        kind="bar",
+        x="unique_values",
+        y="counts",
+        color="skyblue",
+        legend=False,
+    )
+
+    ax.set_xlabel("Tempo")
+    ax.set_ylabel("Count")
+    ax.set_title("Tempo Counts")
+    ax.set_xticks(
+        np.arange(
+            0,
+            203,
+            step=5,
+        )
+    )
+    ax.set_xlim(
+        0,
+        tempo_counts["unique_values"].max()
+        - tempo_counts["unique_values"].min()
+        - 11,
+    )
+
+    plt.show(ax)
+    plt.savefig("output/Q3_tempo_counts")
+    plt.close()
+
+    ax = key_counts.plot(
+        kind="bar",
+        x="unique_values",
+        y="counts",
+        color="skyblue",
+        legend=False,
+    )
+
+    ax.set_xlabel("Key")
+    ax.set_ylabel("Count")
+    ax.set_title("Key Counts")
+    ax.set_xticks(
+        np.arange(
+            -1,
+            12,
+        )
+    )
+    ax.set_xlim(-1, key_counts["unique_values"].max() + 1)
+
+    plt.show(ax)
+    plt.savefig("output/Q3_key_counts")
+    plt.close()
+
+    ax = duration_counts.plot(
+        kind="bar",
+        x="unique_values",
+        y="counts",
+        color="skyblue",
+        legend=False,
+    )
+
+    ax.set_xlabel("Duration (seconds)")
+    ax.set_ylabel("Count")
+    ax.set_title("Duration Counts")
+    ax.set_xticks(
+        np.arange(
+            0,
+            600,
+            step=10,
+        )
+    )
+
+    ax.set_xlim(
+        0,
+        220,
+    )
+
+    plt.show(ax)
+    plt.savefig("output/Q3_duration_counts")
+    plt.close()
+    
+def main():
+    Q1_analysis()
+
+if __name__ =
--- final submission/cleaning.py
+++ final submission/cleaning.py
@@ -1,21 +1,8 @@
 import pandas as pd
+from pandas import DataFrame
 import matplotlib.pyplot as plt
 import seaborn as sns
 from ast import literal_eval
-
-
-def get_Q1_df() -> pd.DataFrame:
-    main_df, stream_df = Q1_data_loading()
-
-    merged_df = Q1_data_cleaning(main_df, stream_df)
-    return merged_df
-
-
-def Q1_data_loading() -> tuple[pd.DataFrame, pd.DataFrame]:
-    main_df = pd.read_csv("data/spotify_songs.csv")
-    stream_df = pd.read_csv("data/stream_data.csv", sep="#")
-
-    return main_df, stream_df
 
 
 def Q1_data_cleaning(main_df, stream_df):
@@ -53,3 +40,83 @@ def Q1_data_cleaning(main_df, stream_df):
     )
 
     return merged_df
+
+
+def Q1_data_loading() -> tuple[DataFrame, DataFrame]:
+    main_df = pd.read_csv("data/spotify_songs.csv")
+    stream_df = pd.read_csv("data/stream_data.csv", sep="#")
+
+    return main_df, stream_df
+
+
+def get_Q1_df() -> DataFrame:
+    main_df, stream_df = Q1_data_loading()
+
+    merged_df = Q1_data_cleaning(main_df, stream_df)
+    return merged_df
+
+
+def Q2_data_cleaning(df) -> tuple[DataFrame, list]:
+    df = df.drop(df.columns[0], axis=1)
+
+    important_cols = ["Age", "fav_music_genre"]
+    important_data = df[important_cols]
+
+    important_age_groups = ["12-20", "20-35", "35-60"]
+    age_group_filter = important_data["Age"].isin(important_age_groups)
+    important_data = important_data[age_group_filter]
+
+    return important_data, important_age_groups
+
+
+def Q2_data_loading() -> DataFrame:
+    return pd.read_csv("data/user_questions.csv")
+
+
+def get_Q2_df() -> tuple[DataFrame, list]:
+    df = Q2_data_loading()
+
+    return Q2_data_cleaning(df)
+
+
+def Q3_data_cleaning(df) -> tuple[DataFrame, DataFrame, DataFrame]:
+    tempo_df = df["tempo"]
+    key_df = df["key"]
+    duration_df = df["duration_ms"]
+
+    tempo_counts = (
+        tempo_df.astype(int)
+        .value_counts(sort=False)
+        .rename_axis("unique_values")
+        .reset_index(name="counts")
+        .sort_values(by="unique_values")
+    )
+
+    key_counts = (
+        key_df.value_counts(sort=False)
+        .rename_axis("unique_values")
+        .reset_index(name="counts")
+        .sort_values(by="unique_values")
+    )
+
+    duration_counts = (
+        duration_df.round(-3)
+        .divide(1000)
+        .astype(int)
+        .value_counts(sort=False)
+        .rename_axis("unique_values")
+        .reset_index(name="counts")
+        .sort_values(by="unique_values")
+    )
+
+    return tempo_counts, key_counts, duration_counts
+
+
+def Q3_data_loading() -> DataFrame:
+    return pd.read_csv("data/1000songdata.csv")
+
+
+def get_Q3_dfs():
+    df = Q3_data_loading()
+
+    return Q3_data_cleaning(df)
